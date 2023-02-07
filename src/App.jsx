be5ea6a5eff7bda9b0068f1c670b8902816f9ebe
luca-lagos <lagos.herrera.luca@gmail.com>
2023-02-06 22:32:33 -0300

    Upload first structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import TodoApp from "./components/TodoApp";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoApp></TodoApp>
+    </div>
+  );
+}
+
+export default App;
